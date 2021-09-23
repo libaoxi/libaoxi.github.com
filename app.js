9d4072390e77175f6a2471b161a0e06ffe31ae7a
libaoxi <i@libaoxi.com>
2021-09-23 23:10:53 +0800

    first

--- app.js
+++
@@ -1,7 +0,0 @@
-const Koa = require('koa')
-const app = new Koa()
-
-app.use(require('koa-static')(__dirname + '/public'))
-app.listen(3000, () => {
-  console.log('app is listening 3000')
-})
---
+++ content/blog/vboxmanager.md
@@ -0,0 +1,10 @@
+---
+title: "Vbox manager"
+date: 2021-09-23T19:10:52+08:00
+tags: ['virtualbox']
+draft: false
+---
+
+```
+VBoxManage list vms
+```
